75cdccbe33d496921e5c95aa6e1166a6262088fa
lanyi <lanyi@ra3.moe>
2019-01-25 18:15:23 +0100

    Simplified code

--- AptEditor.cpp
+++ AptEditor.cpp
@@ -1,12 +1,12 @@
+#include "tinyxml2.h"
 #include <cctype>
 #include <sstream>
-#include "tinyxml2.h"
 
 #include "AptEditor.hpp"
 
 #include "AptConstFile.hpp"
-#include "AptTypes.hpp"
 #include "AptTypeDefinitionsParser.hpp"
+#include "AptTypes.hpp"
 
 #include "Util.hpp"
 
@@ -16,7 +16,7 @@ namespace Apt::AptEditor {
 
 using namespace AptTypes;
 
-template<typename... Arguments>
+template <typename... Arguments>
 std::string makeString(Arguments&&... arguments) {
     auto destination = std::ostringstream{};
     (destination << ... << std::forward<Arguments>(arguments));
@@ -27,12 +27,24 @@ std::string escape(const std::string_view src) {
     auto destination = std::ostringstream{};
     for (char ch : src) {
         switch (ch) {
-            case '&': destination << "&amp;"; break;
-            case '\'': destination << "&apos;"; break;
-            case '"': destination << "&quot;"; break;
-            case '<': destination << "&lt;"; break;
-            case '>': destination << "&gt;"; break;
-            default: destination << ch; break;
+        case '&':
+            destination << "&amp;";
+            break;
+        case '\'':
+            destination << "&apos;";
+            break;
+        case '"':
+            destination << "&quot;";
+            break;
+        case '<':
+            destination << "&lt;";
+            break;
+        case '>':
+            destination << "&gt;";
+            break;
+        default:
+            destination << ch;
+            break;
         }
     }
     return destination.str();
@@ -51,7 +63,7 @@ void writeObject(tinyxml2::XMLElement* node, const AptObjectPool& pool,
         writeNode(node, pool, name, value);
     };
     std::visit(visitor, object.value);
-    if(object.typeName != object.baseTypeName) {
+    if (object.typeName != object.baseTypeName) {
         const auto& [typeTag, derivedTypeMap] =
             pool.types.at(object.baseTypeName).derivedTypes.value();
         node->SetAttribute(typeTag.c_str(), object.typeName.c_str());
@@ -62,57 +74,66 @@ template <>
 void writeNode<std::string>(tinyxml2::XMLElement* node, const AptObjectPool& pool,
                             const std::string& name, const std::string& value) {
     const auto* attributeName = name.empty() ? "value" : name.c_str();
-    node->SetAttribute(attributeName, escape(value).c_str());
+    node->SetAttribute(attributeName, value.c_str());
+}
+
+std::string escapeType(const std::string_view pointedType) {
+    const auto checkForDiscard = [](const char character) {
+        return character == '>' or std::isspace(character);
+    };
+    auto escapedType = std::string{};
+    for (const auto character : pointedType) {
+        if (checkForDiscard(character)) {
+            if (not escapedType.empty() and checkForDiscard(escapedType.back())) {
+                continue;
+            }
+            escapedType.push_back(' ');
+            continue;
+        }
+        escapedType.push_back(character);
+    }
+    return escapedType;
+}
+
+void insertCommentBeforeOtherElements(tinyxml2::XMLElement* node,
+                                      tinyxml2::XMLComment* comment) {
+    auto* firstChild = node->FirstChild();
+    if (firstChild == nullptr or firstChild->ToComment() == nullptr) {
+        node->InsertFirstChild(comment);
+    }
+    else {
+        auto* lastComment = firstChild->ToComment();
+        while (lastComment != nullptr) {
+            auto* next = lastComment->NextSibling();
+            if (next == nullptr or next->ToComment() == nullptr) {
+                break;
+            }
+            lastComment = next->ToComment();
+        }
+        node->InsertAfterChild(lastComment, comment);
+    }
 }
 
 template <>
 void writeNode<AptTypePointer>(tinyxml2::XMLElement* node, const AptObjectPool& pool,
                                const std::string& name, const AptTypePointer& value) {
 
-    const auto transformType = [](const std::string_view string) {
-        auto result = std::string{};
-        for(const auto character : string) {
-            if(character == '>') {
-                result += '-';
-                continue;
-            }
-            if(std::isspace(character)) {
-                continue;
-            }
-            result += character;
-        }
-        return result;
-    };
-
     const auto* attributeName = name.empty() ? "value" : name.c_str();
     const auto valueString =
-        makeString("Pointer-", transformType(value.typePointedTo), " ", value.address);
-
-    node->SetAttribute(attributeName, escape(valueString).c_str());
+        makeString("Pointer ", escapeType(value.typePointedTo), " ", value.address);
+    node->SetAttribute(attributeName, valueString.c_str());
     if (value.typePointedTo == "String") {
         if (const auto found = pool.objectInstances.find(value.address);
             found != pool.objectInstances.end()) {
             const auto& stringPointed = std::get<std::string>(found->second.value);
             const auto* hintValue =
                 stringPointed.empty() ? "(empty)" : stringPointed.c_str();
-            const auto stringHint = makeString("Address ", value.address, " = ", hintValue);
+            const auto stringHint =
+                makeString("Address ", value.address, " = ", hintValue);
             auto* stringHintComment =
                 node->GetDocument()->NewComment(escape(stringHint).c_str());
-            //append comment
-            auto* firstChild = node->FirstChild();
-            if(firstChild == nullptr or firstChild->ToComment() == nullptr) {
-                node->InsertFirstChild(stringHintComment);
-            }
-            else {
-                auto* lastComment = firstChild->ToComment();
-                while (lastComment != nullptr) {
-                    if (lastComment->NextSibling()->ToComment() == nullptr) {
-                        break;
-                    }
-                    lastComment = lastComment->NextSibling()->ToComment();
-                }
-                node->InsertAfterChild(lastComment, stringHintComment);
-            }
+            // append comment
+            insertCommentBeforeOtherElements(node, stringHintComment);
         }
     }
 }
@@ -140,8 +161,6 @@ void writeNode<AptType::MemberArray>(tinyxml2::XMLElement* node,
         }
         writeObject(currentNode, pool, memberName, member);
     }
-
-    /*return writeNode(node, name, value.value);*/
 }
 
 std::string aptToXml(const FileSystem::path aptFileName) {
@@ -177,7 +196,7 @@ std::string aptToXml(const FileSystem::path aptFileName) {
     auto* aptDataNode = xml.NewElement("ParsedAptData");
     xml.InsertEndChild(aptDataNode);
     auto* parent = aptDataNode;
-    auto arrayEnd = Address{0};
+    auto arrayEnd = Address{ 0 };
     for (const auto& [address, object] : pool.objectInstances) {
         if (const auto arrayMarker = pool.arrays.find(address);
             arrayMarker != pool.arrays.end()) {
@@ -209,21 +228,19 @@ std::string aptToXml(const FileSystem::path aptFileName) {
         parent->InsertEndChild(node);
         node->SetAttribute("address", address);
         writeObject(node, pool, {}, object);
-        
 
-        if((parent != aptDataNode) and (arrayEnd <= address + object.size())) {
+        if ((parent != aptDataNode) and (arrayEnd <= address + object.size())) {
             parent = aptDataNode;
-            arrayEnd = 0; //reset array end
+            arrayEnd = 0; // reset array end
         }
     }
-    
 
     tinyxml2::XMLPrinter printer;
     xml.Accept(&printer);
-    const auto xmlString = std::string{printer.CStr()};
+    const auto xmlString = std::string{ printer.CStr() };
     auto xmlOutput = std::ofstream{ aptFileName.string() + ".edited.xml" };
     xmlOutput << xmlString << std::endl;
 
     return "";
 }
-}
+} // namespace Apt::AptEditor
